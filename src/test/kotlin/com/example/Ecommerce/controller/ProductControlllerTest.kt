517fdec3697bb714994f5f88c0b5fba04414b5cb
AMANsharma12356 <imamansharma121@gmail.com>
2022-09-15 16:51:46 +0530

    hi

--- src/test/kotlin/com/example/Ecommerce/controller/ProductControlllerTest.kt
+++ src/test/kotlin/com/example/Ecommerce/controller/ProductControlllerTest.kt
@@ -1,4 +1,5 @@
 
+import arrow.core.mapOf
 import com.example.Ecommerce.controller.ProductController
 import com.example.Ecommerce.model.Product
 import com.example.Ecommerce.repository.ProductRepository
@@ -18,6 +19,7 @@ import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
 import org.springframework.test.web.reactive.server.returnResult
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @WebFluxTest(ProductController::class)
 @AutoConfigureWebTestClient
@@ -27,49 +29,35 @@ class ProductControlllerTest {
     lateinit var client: WebTestClient
 
     @Autowired
-    lateinit var productService: ProductService
-
-    @Autowired
-    lateinit var productRepository: ProductRepository
+    lateinit var producTestCotService: ProductService
 
     @TestConfiguration
-    class ControllerTestConfig {
+    class ControllerTestConfig{
         @Bean
         fun productService() = mockk<ProductService>()
-
-        @Bean
-        fun productRepository() = mockk<ProductRepository>()
-
     }
 
     @Test
-
-    fun `should return list  `() {
-        val product = Product(
-            "12", "aman", "123"
+    fun `should product when create api is being called`(){
+        val expectedResponse = listOf(
+           mapOf( "productId" to "111",
+            "productName" to "Nokia",
+               "productPrice" to "15000"),
+//
         )
-
-        val expectedResult = mapOf(
-            "productId" to "12",
-            "productName" to "aman",
-            "productPrice" to "123"
-        )
+val product1 = Product(
+    "111","Nokia","15000"
+)
         every {
-            productService.findAllproducts()
-        } returns Flux.just(product)
-
-        val response = client.get()
-            .uri("/products/list")
-            .accept(MediaType.APPLICATION_JSON)
-            .exchange() //invoking the end point
-            .expectStatus().is2xxSuccessful
-            .returnResult<Any>()
-            .responseBody
-
-        response.blockFirst() shouldBe expectedResult
-
-        verify(exactly = 1) {
-            productService.findAllproducts()
-        }
+            productService.addProduct(product1)
+        } returns Mono.just(product1)
     }
+    val response = client.post()
+        .uri("/product")
+        .bodyValue(product1)
+        .exchange()
+        .expectStatus().is2xxSuccessful
+        .returnResult<Any>().responseBody
+
+
 }
